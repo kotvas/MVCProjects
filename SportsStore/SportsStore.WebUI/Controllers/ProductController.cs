34f73e3f1e46a3e3a31cc35528b8652ec0dfdcea
kotv17@gmail.com <kotv17@gmail.com>
2016-01-01 18:50:27 +0200

    List action was changed to show data from DB with paging

--- SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,14 +11,17 @@ namespace SportsStore.WebUI.Controllers
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public Int32 PageSize = 4;
+
         public ProductController(IProductRepository productRepository)
         {
             this.repository = productRepository;
         }
 
-        public ViewResult List()
+        public ViewResult List(Int32 page = 1)
         {
-            return View(repository.Products);
+            IEnumerable<Product> products = repository.Products;
+            return View(products.OrderBy(p => p.ProductID).Skip((page - 1) * PageSize).Take(PageSize));
         }
 	}
 }
