079d4c3f6ff4585f0400cd4326c22ecbe169ec7d
kotv17@gmail.com <kotv17@gmail.com>
2016-01-01 16:26:19 +0200

    Product entity was added

---
+++ SportsStore/SportsStore.Domain/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.Entities
+{
+    public class Product
+    {
+        public Int32 ProductID { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public Decimal Price { get; set; }
+        public String Category { get; set; }
+    }
+}
