0e9efed14144e311713fd950360ba1ccbb2de86d
kotv17@gmail.com <kotv17@gmail.com>
2016-01-01 18:49:35 +0200

    Can_Paginate test was added

---
+++ SportsStore/SportsStore.UnitTests/Products.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.UnitTests
+{
+    /// <summary>
+    /// Summary description for Products
+    /// </summary>
+    [TestClass]
+    public class Products
+    {
+        [TestMethod]
+        public void Can_Paginate()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+                {
+                    new Product { ProductID = 1, Name = "P1" },
+                    new Product { ProductID = 2, Name = "P2" },
+                    new Product { ProductID = 3, Name = "P3" },
+                    new Product { ProductID = 4, Name = "P4" },
+                    new Product { ProductID = 5, Name = "P5" }
+                });
+        }
+    }
+}
