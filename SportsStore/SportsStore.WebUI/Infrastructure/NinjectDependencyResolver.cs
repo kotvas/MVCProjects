f0fe78b879adbe626cce91f806960bf09593a80a
kotv17@gmail.com <kotv17@gmail.com>
2016-01-01 16:41:20 +0200

    Mock for IProductRepository was added

--- SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,4 +1,7 @@
-﻿using Ninject;
+﻿using Moq;
+using Ninject;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,7 +32,14 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
-
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+                {
+                    new Product { Name = "Football", Price = 25 },
+                    new Product {Name = "Surf board", Price = 179 },
+                    new Product {Name = "Running shoes", Price = 95 }
+                });
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
         }
     }
 }
