a53646f0a3b218422b8f91bcb099b1a77b651a4f
kotv17@gmail.com <kotv17@gmail.com>
2016-01-02 21:57:52 +0200

    Routes for category were added

--- SportsStore/SportsStore.WebUI/App_Start/RouteConfig.cs
+++ SportsStore/SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -13,16 +13,47 @@ namespace SportsStore.WebUI
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: null,
-                url: "Page{page}",
-                defaults: new { Controller = "Product", action = "List" });
+            routes.MapRoute(null,
+                "",
+                new
+                {
+                    controller = "Product",
+                    action = "List",
+                    category = (String)null,
+                    page = 1
+                });
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Product", action = "List", id = UrlParameter.Optional }
-            );
+            routes.MapRoute(null,
+                "Page{page}",
+                new
+                {
+                    controller = "Product",
+                    action = "List",
+                    category = (String)null
+                });
+
+            routes.MapRoute(null,
+                "{category}",
+                new
+                {
+                    controller = "Product",
+                    action = "List",
+                    page = 1
+                });
+
+            routes.MapRoute(null,
+                "{category}/Page{page}",
+                new
+                {
+                    controller = "Product",
+                    action = "List"
+                },
+                new
+                {
+                    page = @"\d+"
+                });
+
+            routes.MapRoute(null, "{controller}/{action}");
         }
     }
 }
