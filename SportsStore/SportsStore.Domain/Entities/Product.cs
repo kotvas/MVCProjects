e50323ffeeada24ee75d6330e97778e039e5ac24
kotv17@gmail.com <kotv17@gmail.com>
2016-01-15 00:19:55 +0200

    Server validation was added for Product

--- SportsStore/SportsStore.Domain/Entities/Product.cs
+++ SportsStore/SportsStore.Domain/Entities/Product.cs
@@ -12,11 +12,19 @@ namespace SportsStore.Domain.Entities
     {
         [HiddenInput(DisplayValue = false)]
         public Int32 ProductID { get; set; }
+
+        [Required(ErrorMessage = "Please enter a product name")]
         public String Name { get; set; }
 
         [DataType(DataType.MultilineText)]
+        [Required(ErrorMessage = "Please enter a description")]
         public String Description { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
         public Decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Please specify a category")]
         public String Category { get; set; }
     }
 }
