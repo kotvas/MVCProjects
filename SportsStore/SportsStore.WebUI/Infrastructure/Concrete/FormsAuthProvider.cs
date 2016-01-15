d5d2c647bdb12803659e2af655310d2d71236636
kotv17@gmail.com <kotv17@gmail.com>
2016-01-16 00:59:36 +0200

    Authentication functionality was added

---
+++ SportsStore/SportsStore.WebUI/Infrastructure/Concrete/FormsAuthProvider.cs
@@ -0,0 +1,26 @@
+﻿using SportsStore.WebUI.Infrastructure.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace SportsStore.WebUI.Infrastructure.Concrete
+{
+    public class FormsAuthProvider : IAuthProvider
+    {
+
+        public Boolean Authenticate(string userName, string password)
+        {
+            Boolean result = FormsAuthentication.Authenticate(userName, password);
+
+            if (result)
+            {
+                FormsAuthentication.SetAuthCookie(userName, false);
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ SportsStore/SportsStore.WebUI/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SportsStore.WebUI.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public String UserName { get; set; }
+
+        [Required]
+        public String Password { get; set; }
+    }
+}
