a8c377f8c0d4cfec55292ec633ac7bac6e6171a3
kotv17@gmail.com <kotv17@gmail.com>
2016-01-22 09:50:50 +0200

    DebuggingDemo project was added

---
+++ DebuggingDemo/DebuggingDemo/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DebuggingDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            Int32 firstVal = 10;
+            Int32 secondValue = 0;
+
+            Int32 result = firstVal / 2;
+
+            //ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View(result);
+        }
+    }
+}
